11d96fc04fcd5108f273cf54a245111069f97259
heather frank <sjcswank@gmail.com>
2024-03-12 15:26:32 -0500

    Lifecycle Tests

---
+++ src/test/java/junit5tests/FirstTestClass.java
@@ -0,0 +1,39 @@
+package junit5tests;
+
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class FirstTestClass {
+
+    @BeforeAll
+    void beforeAll() {
+        System.out.println("--This is the before all method.");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("----This is the before each method.");
+    }
+
+    @AfterAll
+    void afterAll() {
+        System.out.println("--This is the after all method.");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("----This is the after each method.");
+    }
+
+    @Test
+    void firstMethod() {
+        System.out.println("This is the first test method.");
+    }
+
+    @Test
+    @DisplayName("US1234 - TC12 - this method is the second one.")
+    void secondMethod() {
+        System.out.println("This is the second test method.");
+    }
+
+}
